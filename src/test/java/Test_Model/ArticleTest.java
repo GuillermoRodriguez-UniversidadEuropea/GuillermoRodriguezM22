4609f7a6d69add3aadbfffa16769d1bdf61f3b25
grodriguezg06 <22426062@live.uem.es>
2025-10-12 17:25:57 +0200

    Ej modelo de datos

--- src/test/java/Test_Model/ArticleTest.java
+++ src/test/java/Test_Model/ArticleTest.java
@@ -6,14 +6,30 @@ import org.junit.jupiter.api.Test;
 
 import com.example.model.Article;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+
 
 public class ArticleTest {
     
 
+    private Article article;
 
+    @BeforeEach
+    void setup() {
+        article = new Article("Pan", 5, 2.0, 10);
+    }
 
+    @Test
+    void testGetGrossAmount() {
+        assertEquals(10.0, article.getGrossAmount(), 0.01);
+    }
 
+    @Test
+    void testGetDiscountedAmount() {
+        assertEquals(9.0, article.getDiscountedAmount(), 0.01);
+    }
 }
+
+
+
+
+
